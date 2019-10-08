9f21da9e1ec80cda1efa7093864e0d4e232a014c
Unknown <jaylen_jennings@yahoo.com>
2019-10-08 15:59:15 -0400

    Followed Demo

---
+++ Assets/Jennings/Scripts/ChangeRooms.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jennings {
+    public class ChangeRooms : MonoBehaviour {
+        public Camera cameraThisRoom;
+        public Camera cameraNextRoom;
+        //creates bool preventing me from going to next room
+        public bool gotoNextRoomOnClick = false;
+
+        void OnMouseDown()
+        {
+            //When mouse clicked on object it takes you to next room if gotoNextRoomOnClick is true.
+            if (gotoNextRoomOnClick) GotoNextRoom();
+        }
+
+        public void GotoNextRoom()
+        {
+            cameraThisRoom.gameObject.SetActive(false);
+            cameraNextRoom.gameObject.SetActive(true);
+        }
+    }
+}
