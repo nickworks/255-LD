1fe0f88b9f7166bf87c939a50eac9bc2fea2c22a
Nate <breun@ferris.edu>
2019-10-08 15:49:02 -0400

    10/8 class work

---
+++ Assets/Breu/Week7Demo/Scripts/Door2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BreuWeek7
+{
+
+    [RequireComponent(typeof(changeRooms))]
+    public class Door2 : MonoBehaviour
+    {
+        void OnMouseDown()
+        {
+            if (Inventory.main.hasKey == true)
+            {
+                print("the door is now unlocked");
+                GetComponent<changeRooms>().GoToNextRoom();
+            }
+            else
+            {
+                print("Door is locked");
+            }
+        }
+    }
+}
