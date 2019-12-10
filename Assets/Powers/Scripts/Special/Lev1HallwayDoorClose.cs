c0d4c3c1644ac45ecd9d93afb32f52b55de94997
Aaron <aahproxy@gmail.com>
2019-12-10 14:55:39 -0500

    Fixed massive bugs

---
+++ Assets/Powers/Scripts/Special/Lev1HallwayDoorClose.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Powers
+{
+    public class Lev1HallwayDoorClose : MonoBehaviour
+    {
+        public GameObject player;
+        public Animator lightsOneAnimator;
+        public Animator lightsTwoAnimator;
+        public Animator lightsThreeAnimator;
+        public Animator lightsFourAnimator;
+        public Animator lightsFiveAnimator;
+        public Animator doorTemptingAnimator;
+
+        public GameObject logoReveal;
+        public GameObject loadingPrefab;
+
+        private bool hitTrigger = false;
+        private bool eventStarted = false;
+
+        private void Update()
+        {
+            if (hitTrigger)
+            {
+                if(!eventStarted) StartCoroutine(TransitionEvent());
+                eventStarted = true;
+            }
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider.tag == "Player")
+            {
+                hitTrigger = true;
+            }
+        }
+
+        //This is used to close the door to the outside before dramatically breaking every light. Basically, this just times different animations for objects.
+        IEnumerator TransitionEvent()
+        {
+            doorTemptingAnimator.Play("DoorClose", 0, 0f);
+            yield return new WaitForSeconds(3);
+
+            if (!lightsOneAnimator.GetCurrentAnimatorStateInfo(0).IsName("LightOff")) lightsOneAnimator.Play("LightOff", 0, 0f);
+            yield return new WaitForSeconds(1);
+
+            if (!lightsTwoAnimator.GetCurrentAnimatorStateInfo(0).IsName("LightOff")) lightsTwoAnimator.Play("LightOff", 0, 0f);
+            yield return new WaitForSeconds(1);
+
+            if (!lightsThreeAnimator.GetCurrentAnimatorStateInfo(0).IsName("LightOff")) lightsThreeAnimator.Play("LightOff", 0, 0f);
+            yield return new WaitForSeconds(1);
+
+            if (!lightsFourAnimator.GetCurrentAnimatorStateInfo(0).IsName("LightOff")) lightsFourAnimator.Play("LightOff", 0, 0f);
+            yield return new WaitForSeconds(2);
+
+            if (!lightsFiveAnimator.GetCurrentAnimatorStateInfo(0).IsName("LightOff")) lightsFiveAnimator.Play("LightOff", 0, 0f);
+            yield return new WaitForSeconds(3);
+
+            logoReveal.SetActive(true);
+            player.SetActive(false);
+            yield return new WaitForSeconds(8.5f);
+
+            GameObject loading = Instantiate(loadingPrefab, new Vector3(0, 100, 0), Quaternion.Euler(0, 0, 0));
+            loading.GetComponent<LoadingScript>().sceneName = "Powers_Scene2";
+
+            yield break;
+        }
+    }
+}
---
+++ Assets/Powers/Scripts/Special/Lev1HallwayTeleport.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Powers
+{
+    public class Lev1HallwayTeleport : MonoBehaviour
+    {
+        public GameObject player;
+        public Animator doorTransitionAnimator;
+        public Animator lightsOneAnimator;
+        public Animator lightsTwoAnimator;
+        public Animator lightsThreeAnimator;
+        public Animator lightsFourAnimator;
+        public Animator lightsFiveAnimator;
+        public Animator doorTemptingAnimator;
+
+        private bool hitTrigger = false;
+        private bool eventStarted = false;
+
+        private void Update()
+        {
+            if (hitTrigger)
+            {
+                if (!eventStarted) StartCoroutine(TransitionEvent());
+                eventStarted = true;
+            }
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider.tag == "Player")
+            {
+                hitTrigger = true;
+            }
+        }
+
+        //This is used to close the behind the player, teleport the player, and then one by one activate the lights. Basically, this just times different animations for objects.
+        IEnumerator TransitionEvent()
+        {
+            if (!doorTransitionAnimator.GetCurrentAnimatorStateInfo(0).IsName("DoorClose")) doorTransitionAnimator.Play("DoorClose", 0, 0f);
+            yield return new WaitForSeconds(1.8f);
+            
+            //teleport player to the actual hallway to ensure global illumination from the bright tutorial doesn't interfere.
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 0.1f, player.transform.position.z + 30);
+            yield return new WaitForSeconds(1);
+
+            if (!lightsOneAnimator.GetCurrentAnimatorStateInfo(0).IsName("LightOn")) lightsOneAnimator.Play("LightOn", 0, 0f);
+            yield return new WaitForSeconds(0.5f);
+
+            if (!lightsTwoAnimator.GetCurrentAnimatorStateInfo(0).IsName("LightOn")) lightsTwoAnimator.Play("LightOn", 0, 0f);
+            yield return new WaitForSeconds(0.5f);
+
+            if (!lightsThreeAnimator.GetCurrentAnimatorStateInfo(0).IsName("LightOn")) lightsThreeAnimator.Play("LightOn", 0, 0f);
+            yield return new WaitForSeconds(0.5f);
+
+            if (!lightsFourAnimator.GetCurrentAnimatorStateInfo(0).IsName("LightOn")) lightsFourAnimator.Play("LightOn", 0, 0f);
+            yield return new WaitForSeconds(0.5f);
+
+            if (!lightsFiveAnimator.GetCurrentAnimatorStateInfo(0).IsName("LightOn")) lightsFiveAnimator.Play("LightOn", 0, 0f);
+            yield return new WaitForSeconds(1);
+
+            if (!doorTemptingAnimator.GetCurrentAnimatorStateInfo(0).IsName("DoorOpen")) doorTemptingAnimator.Play("DoorOpen", 0, 0f);
+            yield return new WaitForSeconds(1);
+
+            Destroy(gameObject);
+
+            yield break;
+        }
+    }
+}
